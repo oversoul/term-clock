6f4a83011c49825e3d44a92f5a146953ca156563
Amine <kurosoul.ae@gmail.com>
2022-01-11 18:55:05 +0100

    Set terminal size to center the clock

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,120 @@ use colored::*;
 use std::collections::HashMap;
 use std::time::Duration;
 
-fn main() {
-    let space = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
-    let one = [[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 0, 1], [0, 0, 1]];
-    let two = [[1, 1, 1], [0, 0, 1], [1, 1, 1], [1, 0, 0], [1, 1, 1]];
-    let three = [[1, 1, 1], [0, 0, 1], [1, 1, 1], [0, 0, 1], [1, 1, 1]];
-    let four = [[1, 0, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [0, 0, 1]];
-    let five = [[1, 1, 1], [1, 0, 0], [1, 1, 1], [0, 0, 1], [1, 1, 1]];
-    let six = [[1, 1, 1], [1, 0, 0], [1, 1, 1], [1, 0, 1], [1, 1, 1]];
-    let seven = [[1, 1, 1], [0, 0, 1], [0, 1, 1], [0, 0, 1], [0, 0, 1]];
-    let eight = [[1, 1, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 1, 1]];
-    let nine = [[1, 1, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [1, 1, 1]];
-    let zero = [[1, 1, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1]];
-    let dots = [[0, 0, 0], [0, 1, 0], [0, 0, 0], [0, 1, 0], [0, 0, 0]];
+const BLOCK: &str = "  ";
+type Font = [[u8; 5]; 7];
 
-    let mut numbers: HashMap<char, [[u8; 3]; 5]> = HashMap::new();
+fn main() {
+    let space = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+    let one = [
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+    ];
+    let two = [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1],
+    ];
+    let three = [
+        [1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+    ];
+    let four = [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+    ];
+    let five = [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+    ];
+    let six = [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+    ];
+    let seven = [
+        [1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+    ];
+    let eight = [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+    ];
+    let nine = [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+    ];
+    let zero = [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 1, 1],
+        [1, 0, 1, 0, 1],
+        [1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+    ];
+    let dots = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let mut numbers: HashMap<char, Font> = HashMap::new();
     numbers.insert(':', dots);
     numbers.insert('0', zero);
     numbers.insert('1', one);
@@ -30,12 +129,22 @@ fn main() {
     numbers.insert('8', eight);
     numbers.insert('9', nine);
 
-    // println!("{:?} hour", hour_str.chars());
+    let mut empty_lines = 0;
+    let mut padding_left = 0;
+    let shape = "..... ..... ::::: ..... ..... ::::: ..... .....";
+
+    if let Some((w, h)) = term_size::dimensions() {
+        // println!("Width: {}\nHeight: {}", w, h);
+        empty_lines = (h - 7) / 2;
+        padding_left = ((w / BLOCK.len()) - shape.len()) / 2;
+    }
 
     loop {
         let current_time = chrono::offset::Local::now();
         clear_console();
-        hide_cursor();
+        for _ in 0..empty_lines {
+            println!();
+        }
         let clock = format!(
             "{:0width$}:{:0width$}:{:0width$}",
             current_time.hour(),
@@ -43,16 +152,15 @@ fn main() {
             current_time.second(),
             width = 2
         );
-        println!();
-        let time: String = [0, 1, 2, 3, 4]
-            .iter()
+        let time: String = (0..7)
             .map(|row| {
                 format!(
-                    "{}\n",
+                    "{}{}\n",
+                    BLOCK.repeat(padding_left),
                     clock
                         .chars()
                         .map(|c| numbers.get(&c).unwrap_or(&space))
-                        .map(|num| format!("{}   ", draw_number_line(*num, *row)))
+                        .map(|num| format!("{}{}", draw_number_line(*num, row), BLOCK))
                         .collect::<String>()
                         .trim_end()
                 )
@@ -61,9 +169,6 @@ fn main() {
 
         print!("{}", time);
 
-        /*
-        println!( "{:?} {} {}", current_time.hour(), current_time.minute(), current_time.second());
-        */
         std::thread::sleep(Duration::from_secs(1));
     }
 }
@@ -72,18 +177,14 @@ fn clear_console() {
     print!("\x1B[2J\x1B[1;1H");
 }
 
-fn hide_cursor() {
-    print!("");
-}
-
-fn draw_number_line(number: [[u8; 3]; 5], line: usize) -> String {
+fn draw_number_line(number: Font, line: usize) -> String {
     number[line]
         .iter()
         .map(|c| {
             if *c == 0 {
-                format!("{}", "   ".normal())
+                format!("{}", BLOCK.normal())
             } else {
-                format!("{}", "   ".reverse())
+                format!("{}", BLOCK.reverse().blue())
             }
         })
         .collect::<String>()
